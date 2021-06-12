31a5f8f3f17aca28b74eac8329cfde962aa262f3
shree1326 <83897486+shree1326@users.noreply.github.com>
2021-06-12 17:09:37 +0530

    Add files via upload

---
+++ Task-02/minmax.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Task-2 : minmax
+	</title>
+</head>
+<body>
+	<h2>Minimum and Maximum Program in PHP.</h2><br>	
+	<?php 
+	$max = 40;
+	$min = 10;
+	if ($max < $min) {
+		echo "$max is greater than $min";
+	}
+	else{
+		echo "$min is smaller than $max";
+	}
+	?>
+</body>
+</html>
---
+++ Task-02/subtraction.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Task-2 : Subtraction
+	</title>
+</head>
+<body>
+	<h2>Subtraction Program in PHP.</h2><br>	
+	<?php  
+	$x=60;  
+	$y=15;  
+	$z=$x-$y;  
+	echo "Subtraction of $x-$y=<b>$z.</b>";  
+	?>
+</body>
+</html>
